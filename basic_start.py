9f890be3f6ce5916428edf4b9f18f7a56e7fa2f7
shreyapawar07 <shreyapawark7@gmail.com>
2025-11-24 20:52:58 +0530

    Adding two more files

--- basic_start.py
+++ basic_start.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Path
 import json
 
 app= FastAPI()
@@ -16,3 +16,12 @@ def load_json(file):
 def view():
     data = load_json("patients.json")
     return data
+
+@app.get('/patient/{patient_id}')  # pyright: ignore[reportUndefinedVariable]
+def patient(patient_id:str=Path(...,description='Id of a patient',example='P001')):
+    data =load_json('patients.json')
+    
+    if patient_id in data:
+        return data[patient_id]
+    else:
+        return "No data found"
